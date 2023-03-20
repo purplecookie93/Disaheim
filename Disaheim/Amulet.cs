1fda12baa7a03c7ec30d962e703321aec0735247
Charlotte Meldgaard <charlotte.meldgaard93@gmail.com>
2023-03-20 21:28:29 +0100

    initial push

---
+++ Disaheim/Amulet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    internal class Amulet
+    {
+        public string ItemId { get; set; }
+        public string Design { get; set; }
+
+        public Enum.Level Quality { get; set; }
+
+    }
+}
---
+++ Disaheim/Enum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Enum
+    {
+        public enum Level
+        {
+            low,
+            medium,
+            high
+        }
+    }
+}
